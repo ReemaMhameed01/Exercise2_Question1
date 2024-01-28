50495778ecc4dc7d8df95f2e0862b5fe01fc28da
Reema <Reema@10.0.0.29>
2024-01-29 00:14:24 +0200

    finished question 1 in exercise 2

--- src/main/java/Exe2_Ques1/Exe2_Ques1/App.java
+++ src/main/java/Exe2_Ques1/Exe2_Ques1/App.java
@@ -6,8 +6,24 @@ package Exe2_Ques1.Exe2_Ques1;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static String NumberToChar(int number) {
+		if ((90 <=number ) & (number <=100 ))
+			return "A";
+		else if((80 <=number ) & (number <=89 ))
+			return "B";
+		else if((70 <=number ) & (number <=79 ))
+			return "C";
+		else if((60 <=number ) & (number <=69 ))
+			return "D";
+		else if((50 <=number ) & (number <=59 ))
+			return "E";
+		else if((1 <=number ) & (number <=49 ))
+			return "F";
+		
+		
+		return "error";
+
+
+	}
 }
+
--- src/test/java/Exe2_Ques1/Exe2_Ques1/AppTest.java
+++ src/test/java/Exe2_Ques1/Exe2_Ques1/AppTest.java
@@ -9,12 +9,43 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void exe_2_Partition_A_min() {
+
+		String result = App.NumberToChar(90);
+		String expected_result="A";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+	@Test
+	public void exe_2_Partition_A_min_plus1() {
+
+		String result = App.NumberToChar(91);
+		String expected_result="C";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+	@Test
+	public void exe_2_Partition_A_max() {
+
+		String result = App.NumberToChar(100);
+		String expected_result="A";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+
+	@Test
+	public void exe_2_Partition_A_max_minus_1() {
+
+		String result = App.NumberToChar(99);
+		String expected_result="A";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
+	@Test
+	public void exe_2_Partition_A_Nom() {
+
+		String result = App.NumberToChar(50);
+		String expected_result="A";
+		org.junit.Assert.assertEquals(expected_result,result,0.00001);
+	}
 }
